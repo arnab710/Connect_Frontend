451098691f20b54bdf6873127b3bf18cea85d6a4
Arnab Dey <arnabdey710@gmail.com>
2023-09-20 13:24:12 +0530

    Added Modal Window

--- src/features/EachComment/EachComment.tsx
+++ src/features/EachComment/EachComment.tsx
@@ -1,11 +1,13 @@
 import { AiFillDelete } from "react-icons/ai";
-import React from "react";
+import React, { useState } from "react";
 import { eachCommentType } from "../../Types/AfterFetchComment";
 import style from "./EachComment.module.css";
 import { useAppSelector } from "../../Redux/ReduxAppType/AppType";
+import Modal from "../ModalWindow/Modal";
 
 const EachComment: React.FC<{ eachComment: eachCommentType }> = ({ eachComment }) => {
 	const userID = useAppSelector((state) => state.user._id);
+	const [openModal, setOpenModal] = useState<boolean>(false);
 
 	return (
 		<div className={style.overallCommentDiv}>
@@ -26,10 +28,26 @@ const EachComment: React.FC<{ eachComment: eachCommentType }> = ({ eachComment }
 						<h1 className={style.headName}>
 							{eachComment.user.firstName} {eachComment.user.lastName}
 						</h1>
-						{userID === eachComment.user._id && <AiFillDelete className={style.deleteIcon} />}
+						{userID === eachComment.user._id && (
+							<button onClick={() => setOpenModal((c) => !c)} className={style.deleteBtn}>
+								<AiFillDelete className={style.deleteIcon} />
+							</button>
+						)}
 					</div>
 					<p className={style.userComment}>{eachComment.comment}</p>
 				</div>
+				{openModal && (
+					<Modal onClose={() => setOpenModal(false)}>
+						<div className={style.modalContent}>
+							<h1 className={style.deleteHeader}>Delete Confirmation</h1>
+							<p className={style.deletPara}>Are You Sure You Want To Delete Your Comment ?</p>
+							<div className={style.btnDiv}>
+								<button className={style.cancelBtn}>CANCEL</button>
+								<button className={style.deleteBtn2}>DELETE</button>
+							</div>
+						</div>
+					</Modal>
+				)}
 			</div>
 		</div>
 	);
---
+++ src/features/ModalWindow/Modal.tsx
@@ -0,0 +1,71 @@
+import { createPortal } from "react-dom";
+import { HiXMark } from "react-icons/hi2";
+import styled from "styled-components";
+
+const StyledModal = styled.div`
+	position: fixed;
+	top: 50%;
+	left: 50%;
+	transform: translate(-50%, -50%);
+	background-color: #fff;
+	border-radius: 15px;
+	box-shadow: 0 5px 15px rgba(0, 0, 0, 0.1), 0 3px 6px rgba(0, 0, 0, 0.2);
+	padding: 2rem 1rem;
+	width: 40%;
+	transition: all 0.5s;
+`;
+
+const Overlay = styled.div`
+	position: fixed;
+	top: 0;
+	left: 0;
+	width: 100%;
+	height: 100vh;
+	background-color: rgba(255, 255, 255, 0.1);
+	backdrop-filter: blur(4px);
+	z-index: 1000;
+	transition: all 0.5s;
+`;
+
+const Button = styled.button`
+	background: none;
+	border: none;
+	padding: 0.4rem;
+	border-radius: 5px;
+	transform: translateX(0.8rem);
+	transition: all 0.2s;
+	position: absolute;
+	top: 1.2rem;
+	right: 1.9rem;
+	cursor: pointer;
+
+	&:hover {
+		background-color: #f3f4f6;
+	}
+
+	& svg {
+		width: 1.5rem;
+		height: 1.5rem;
+		/* Sometimes we need both */
+		/* fill: var(--color-grey-500);
+    stroke: var(--color-grey-500); */
+		color: #6b7280;
+	}
+`;
+
+function Modal({ children, onClose }: { children: React.ReactNode; onClose: () => void }) {
+	return createPortal(
+		<Overlay>
+			<StyledModal>
+				<Button onClick={onClose}>
+					<HiXMark />
+				</Button>
+
+				<div>{children}</div>
+			</StyledModal>
+		</Overlay>,
+		document.body
+	);
+}
+
+export default Modal;
