42453afd9c169b09afba0f556dfc71d6a4893d32
Arnab Dey <arnabdey710@gmail.com>
2023-09-20 17:35:40 +0530

    sponsor slider created

---
+++ src/features/RightSponsors/RightSponsorSlider.ts
@@ -0,0 +1,5 @@
+export const sponsorsSlider: string[] = [
+	"https://res.cloudinary.com/dmrlrtwbb/image/upload/v1695207405/michaela-baum-VnM6_liIRJ0-unsplash_r0xru3.jpg",
+	"https://res.cloudinary.com/dmrlrtwbb/image/upload/v1695207405/ella-olsson-uG1jwfpCRhg-unsplash_iw55le.jpg",
+	"https://res.cloudinary.com/dmrlrtwbb/image/upload/v1695210330/najlacam-f8TajCVv4s8-unsplash_ffi74m.jpg",
+];
--- src/features/RightSponsors/RightSponsors.tsx
+++ src/features/RightSponsors/RightSponsors.tsx
@@ -1,8 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import style from "./RightSponsors.module.css";
+import { sponsorsSlider } from "./RightSponsorSlider";
 
 const RightSponsors: React.FC = () => {
-	return <div className={style.backgroundDiv}>this is right sponsors</div>;
+	const [currentPictureIndex, setCurrentPictureIndex] = useState<number>(0);
+
+	useEffect(() => {
+		const intervalID = setInterval(() => {
+			setCurrentPictureIndex((curr) => (curr + 1) % sponsorsSlider.length);
+		}, 3000);
+		return () => clearInterval(intervalID);
+	}, []);
+	return (
+		<div className={style.backgroundDiv}>
+			<div className={style.headDiv}>
+				<span className={style.SponsorHead}>Sponsored</span>
+				<span className={style.createAdHead}>Create Ad</span>
+			</div>
+			<div className={style.imgDiv}>
+				<div className={style.imgOverlay} style={{ transform: `translateX(-${currentPictureIndex * 100}%)` }}>
+					{sponsorsSlider.map((eachVal, index) => (
+						<img src={eachVal} className={style.img} alt="advertisement" key={index} />
+					))}
+				</div>
+			</div>
+			<div className={style.companyNameDiv}>
+				<span className={style.companyName}>SweetCrafts</span>
+				<span className={style.companyWebsite}>www.SweetCrafts.com</span>
+			</div>
+			<p className={style.description}>SweetCrafts: Crafting artisanal desserts. Our bakers use the finest ingredients. Every treat is a work of art. Indulge in sweet perfection.</p>
+		</div>
+	);
 };
 
 export default RightSponsors;
