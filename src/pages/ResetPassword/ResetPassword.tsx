1c33b7ec553a12424653cea460ecc8a3ec5c7cc3
Arnab Dey <arnabdey710@gmail.com>
2023-10-03 22:28:39 +0530

    Some

--- src/pages/ResetPassword/ResetPassword.tsx
+++ src/pages/ResetPassword/ResetPassword.tsx
@@ -1,8 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
 import LoginSlider from "../../features/loginSlider/LoginSlider";
 import ResetPasswordForm from "../../features/ResetPasswordForm/ResetPasswordForm";
+import { useLocation } from "react-router-dom";
 
 const ResetPassword: React.FC = () => {
+	const location = useLocation();
+	useEffect(() => {
+		window.scrollTo(0, 0);
+	}, [location.pathname]);
+
 	return (
 		<div style={{ display: `flex` }}>
 			<LoginSlider />
